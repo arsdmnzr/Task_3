7c9547db3b82a8fe9775c323a8ca7c0ff9b889a0
arsdmnzr <you@example.com>
2023-09-02 11:18:20 +0530

    done

--- content.js
+++ content.js
@@ -1,9 +1,11 @@
 let likeButtons = [...document.querySelectorAll('Like')];
+let likeCount;
 for (let i = 0; i < Math.min(likeCount, likeButtons.length); i++) {
     likeButtons[i].click();
 }
 
 let commentFields = [...document.querySelectorAll('Comment')];
+let commentCount;
 for (let i = 0; i < Math.min(commentCount, commentFields.length); i++) {
     commentFields[i].value = 'CFBR';
     commentFields[i].closest('appropriate-selector-for-comment-form').submit();
